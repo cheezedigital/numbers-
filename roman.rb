2df623cacfec53360dac0e9a239e4501cc6c6333
Justin Veirs <justinveirs@gmail.com>
2014-07-12 23:30:59 -0600

    what the fuck, why isn't this working

--- roman.rb
+++ roman.rb
@@ -1,5 +1,11 @@
 class Roman
 
-    
+  def self.convert(num)
+    if num 1
+      return 'I'
+    else num 2
+      return 'II'
+    end
+  end
 
 end
--- roman_spec.rb
+++ roman_spec.rb
@@ -1,9 +1,17 @@
 require_relative 'spec_helper'
-require_relative 'roman'
+require_relative 'Roman'
 
 describe 'Roman', 'Git Er Don!' do
 
   describe '.convert', :focus do
+
+    it 'takes the number 1 and returns I' do
+      expect(Roman.convert(1)).to eq 'I'
+    end
+
+    it 'takes the number 2 and returns II' do
+      expect(Roman.convert(2)).to eq 'II'
+    end
 
   end
 
