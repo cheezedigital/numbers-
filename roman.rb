6c8532a44ae917950148308ff917d757abc0dbc6
Justin Veirs <justinveirs@gmail.com>
2014-07-13 16:52:48 -0600

    converting roman numerals, however, no refactoring

--- roman.rb
+++ roman.rb
@@ -3,8 +3,30 @@ class Roman
   def self.convert(num)
     if num == 1
       return 'I'
-    else num == 2
+    elsif num == 2
       return 'II'
+    elsif num == 3
+      return 'III'
+    elsif num == 4
+      return 'IV'
+    elsif num == 5
+      return 'V'
+    elsif num == 6
+      return 'VI'
+    elsif num == 7
+      return 'VII'
+    elsif num == 8
+      return 'VIII'
+    elsif num == 9
+      return 'IX'
+    elsif num == 10
+      return 'X'
+    elsif num == 50
+      return 'L'
+    elsif num == 500
+      return 'D'
+    elsif num == 1000
+      return 'M'
     end
   end
 
--- roman_spec.rb
+++ roman_spec.rb
@@ -13,6 +13,50 @@ describe 'Roman', 'Git Er Don!' do
       expect(Roman.convert(2)).to eq 'II'
     end
 
+    it 'takes the number 3 and returns III' do
+      expect(Roman.convert(3)).to eq 'III'
+    end
+
+    it 'takes the number 4 and returns IV' do
+      expect(Roman.convert(4)).to eq 'IV'
+    end
+
+    it 'takes the number 5 and returns V' do
+      expect(Roman.convert(5)).to eq 'V'
+    end
+
+    it 'takes the number 6 and returns VI' do
+      expect(Roman.convert(6)).to eq 'VI'
+    end
+
+    it 'takes the number 7 and returns VII' do
+      expect(Roman.convert(7)).to eq 'VII'
+    end
+
+    it 'takes the number 8 and returns VIII' do
+      expect(Roman.convert(8)).to eq 'VIII'
+    end
+
+    it 'takes the number 9 and returns IX' do
+      expect(Roman.convert(9)).to eq 'IX'
+    end
+
+    it 'takes the number 10 and returns X' do
+      expect(Roman.convert(10)).to eq 'X'
+    end
+
+    it 'takes the number 50 and returns L' do
+      expect(Roman.convert(50)).to eq 'L'
+    end
+
+    it 'takes the number 500 and returns D' do
+      expect(Roman.convert(500)).to eq 'D'
+    end
+
+    it 'takes the number 1000 and returns M' do
+      expect(Roman.convert(1000)).to eq 'M'
+    end
+
   end
 
 end
