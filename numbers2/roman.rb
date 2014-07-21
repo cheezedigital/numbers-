a66330d14566e0598265ab95b17357d8d8e3fb21
Justin Veirs <justinveirs@gmail.com>
2014-07-20 20:01:47 -0600

    very with little looking at previous example, I was able to complete the conversion

--- numbers2/roman.rb
+++ numbers2/roman.rb
@@ -1,6 +1,32 @@
 class Roman
 
-  def self.convert(num)
-
+  def self.converting(num)
+    if num == 1
+      return 'I'
+    elsif num == 2
+      return 'II'
+    elsif num == 3
+      return 'III'
+    elsif num == 4
+      return 'IV'
+    elsif num == 5
+      return 'V'
+    elsif num == 6
+      return 'VI'
+    elsif num == 7
+      return 'VII'
+    elsif num == 8
+      return 'VIII'
+    elsif num == 9
+      return 'IX'
+    elsif num == 10
+      return 'X'
+    elsif num == 50
+      return 'L'
+    elsif num == 550
+      return 'DL'
+    else num == num
+      return 'num'
+    end
   end
 end
--- numbers2/roman_spec.rb
+++ numbers2/roman_spec.rb
@@ -1,13 +1,57 @@
 require_relative 'spec_helper'
 require_relative 'Roman'
 
-describe 'Roman', Converting Numbers to Roman Numerals do
+describe 'Roman', 'Converting Numbers Roman Numerals' do
 
   describe '.converting', :focus do
     it 'takes the numeber 1 and returns I' do
       expect(Roman.converting(1)).to eq 'I'
     end
 
+    it 'takes the number 2 and returns II' do
+      expect(Roman.converting(2)).to eq 'II'
+    end
+
+    it 'takes the number 3 and returns III' do
+      expect(Roman.converting(3)).to eq 'III'
+    end
+
+    it 'takes the number 4 and returns IV' do
+      expect(Roman.converting(4)).to eq 'IV'
+    end
+
+    it 'takes the number 5 and returns V' do
+      expect(Roman.converting(5)).to eq 'V'
+    end
+
+    it 'takes the number 6 and returns VI' do
+      expect(Roman.converting(6)).to eq 'VI'
+    end
+
+    it 'takes the number 7 and returns VII' do
+      expect(Roman.converting(7)).to eq 'VII'
+    end
+
+    it 'takes the number 8 and returns VIII' do
+      expect(Roman.converting(8)).to eq 'VIII'
+    end
+
+    it 'takes the number 9 and returns IX' do
+      expect(Roman.converting(9)).to eq 'IX'
+    end
+
+    it 'takes the number 10 and returns X' do
+      expect(Roman.converting(10)).to eq 'X'
+    end
+
+    it 'takes the number 50 and returns L' do
+      expect(Roman.converting(50)).to eq 'L'
+    end
+
+    it 'takes the number 550 and returns DL' do
+      expect(Roman.converting(550)).to eq 'DL'
+    end
+    
   end
 
 
